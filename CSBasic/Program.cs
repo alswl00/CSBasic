b501905466f59c4accc0a2fd57e863a497d0d45b
Minji Kim <s2018s24@e-mirim.hs.kr>
2020-04-28 14:21:53 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,28 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "윤" + "인" + "성";
+            Console.Write("Hello C#");
+            //주석
+            /*여러줄
+             * 주석*/
+            Console.WriteLine("Hello World");
+            Console.Write("Hello world");
+            Console.Write("Hello world");
+
+            Console.Write(52);
+            Console.Write(7 % 3);
+            Console.Write('A');
+            Console.Write("A");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+            Console.WriteLine("가" + "나" + "다");
+
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
         }
     }
 }
